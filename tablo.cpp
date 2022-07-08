fe332eb57da7fcabf376aaeff62f68920eceb062
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-07-08 17:26:01 +0200

    initialized with functions added function "definition"

--- tablo.cpp
+++ tablo.cpp
@@ -1,43 +1,49 @@
 ﻿#include <iostream>
 
+int definition(int);
+
 int main() {
 	int n = 0;
 	std::cout << "Enter number of seconds" << std::endl;
 	do {
 		std::cin >> n;
-		if (n < 0 || n > 28800) {
-			std::cout << "it's not working time now" << std::endl;
-		}
-		else if (n == 28800) {
-			std::cout << "8 hours left" << std::endl;
-		}
-		else if (25200 <= n && n < 28800) {
-			std::cout << "7 hours left" << std::endl;
-		}
-		else if (21600 <= n && n < 25200) {
-			std::cout << "6 hours left" << std::endl;
-		}
-		else if (18000 <= n && n < 21600) {
-			std::cout << "5 hours left" << std::endl;
-		}
-		else if (14400 <= n && n < 18000) {
-			std::cout << "4 hours left" << std::endl;
-		}
-		else if (10800 <= n && n < 14400) {
-			std::cout << "3 hours left" << std::endl;
-		}
-		else if (7200 <= n && n < 10800) {
-			std::cout << "2 hours left" << std::endl;
-		}
-		else if (3600 <= n && n < 7200) {
-			std::cout << "1 hour left" << std::endl;
-		}
-		else if (0 < n && n < 3600) {
-			std::cout << "less than an hour left" << std::endl;
-		}
-		else if (n == 0) {
-			std::cout << "It's time to relax!" << std::endl;
-		}
+		definition(n);
 	} while (n != 0);
+	return 0;
+}
+int definition(int a) {
+	if (a < 0 || a > 28800) {
+		std::cout << "it's not working time now" << std::endl;
+	}
+	else if (a == 28800) {
+		std::cout << "8 hours left" << std::endl;
+	}
+	else if (25200 <= a && a < 28800) {
+		std::cout << "7 hours left" << std::endl;
+	}
+	else if (21600 <= a && a < 25200) {
+		std::cout << "6 hours left" << std::endl;
+	}
+	else if (18000 <= a && a < 21600) {
+		std::cout << "5 hours left" << std::endl;
+	}
+	else if (14400 <= a && a < 18000) {
+		std::cout << "4 hours left" << std::endl;
+	}
+	else if (10800 <= a && a < 14400) {
+		std::cout << "3 hours left" << std::endl;
+	}
+	else if (7200 <= a && a < 10800) {
+		std::cout << "2 hours left" << std::endl;
+	}
+	else if (3600 <= a && a < 7200) {
+		std::cout << "1 hour left" << std::endl;
+	}
+	else if (0 < a && a < 3600) {
+		std::cout << "less than an hour left" << std::endl;
+	}
+	else if (a == 0) {
+		std::cout << "It's time to relax!" << std::endl;
+	}
 	return 0;
 }
