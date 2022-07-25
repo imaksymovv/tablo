b6ebc4996acf79f0a676f48e267e710eb362a72e
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-07-25 14:32:03 +0200

    reworked tablo.cpp
    switched / to /=
    renamed function definition to hours_conversion

--- tablo.cpp
+++ tablo.cpp
@@ -1,24 +1,24 @@
 ﻿#include <iostream>
 
-int definition(int);
+int seconds_conversion(int);
 
 int main() {
 	int n = 0;
 	std::cout << "Enter number of seconds" << std::endl;
 	do {
 		std::cin >> n;
-		definition(n);
+		seconds_conversion(n);
 	} while (n != 0);
 	std::cout << "its time to relax!" << std::endl;
 	return 0;
 }
-int definition(int a) {
+int seconds_conversion(int a) {
 	int result = 0;
 	if (a > 28800 || a < 0) {
 		std::cout << "its not working time now" << std::endl;
 	}
 	else {
-		a = a / 3600;
+		a /= 3600;
 		std::cout << a << " hours left" << std::endl;
 	}
 	return result;
